4c1c0e39b4d0b8e20c30542ac3487f4266dc69f8
Austin Kregel <austin@kbcomp.co>
2019-10-13 19:55:34 -0400

    Delete 9.activate-new-release.sh

--- .scripts/9.activate-new-release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source /home/ubuntu/.deploy-APPLICATION_NAME-env
-cd "$DEPLOY_BASE_DIRECTORY"
-
-rm "$DEPLOY_BASE_DIRECTORY/current"
-ln -svf "$DEPLOY_DIRECTORY/$DEPLOY_NAME" "$DEPLOY_BASE_DIRECTORY/current"
-
